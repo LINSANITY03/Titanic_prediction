7a37d76c223e3f9c50f238d270fc7ad8baa89463
pujan tamang <pujantamang92@gmail.com>
2023-09-16 12:09:52 +0100

    added import and dataset using pandas

---
+++ prediction_main.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+import tensorflow as tf
+
+# Load dataset.
+dftrain = pd.read_csv(
+    'https://storage.googleapis.com/tf-datasets/titanic/train.csv')  # training data
+dfeval = pd.read_csv(
+    'https://storage.googleapis.com/tf-datasets/titanic/eval.csv')  # testing data
+y_train = dftrain.pop('survived')  # get survived column data from dataframe
+y_eval = dfeval.pop('survived')  # get survived column data from dataframe
+
+print(tf.__version__)  # tf version 2.13.0
+print(dftrain.head())  # return 5 items in our dataframe
+print(dftrain.describe())  # getting statistical data
+print(dftrain.shape)  # (627, 9) getting the rows and column
+print(y_train.head())  # bool representation of survived in true or false
